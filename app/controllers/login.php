63fc606324ce2562c37e05936ceb9b94ff88aed7
Nzola Kiampava <nzolakiampava@gmail.com>
2022-11-15 23:01:51 -1200

    Update login.php

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -6,13 +6,6 @@ Class Login extends Controller
 	public function index()
 	{
 		$data['page_title'] = "Login";
-
-		if($_SERVER['REQUEST_METHOD'] == "POST")
-		{
-			//show($_POST);
-			$user = $this->load_model("user");
-			$user->login($_POST);
-		}
 		$this->view("login", $data);
 	}
 
