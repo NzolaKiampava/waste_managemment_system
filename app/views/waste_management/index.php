a74bd63b9663a4950c08f0c49bad42280028a781
Nzola Kiampava <nzolakiampava@gmail.com>
2022-11-17 10:39:44 +0100

    Add files via upload

---
+++ app/views/waste_management/index.php
@@ -0,0 +1,171 @@
+<?php $this->view("header", $data);?>
+
+      <section class="py-0 bg-light-gradient">
+        <div class="bg-holder" style="background-image:url(<?=ASSETS . THEME?>/assets/img/illustrations/hero-bg.png);background-position:top right;background-size:contain;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 col-md-5 order-md-1 pt-8"><img class="img-fluid" src="<?=ASSETS . THEME?>/assets/img/gallery/webimg.webp" width="400" alt="" /></div>
+            <div class="col-md-7 col-lg-6 text-center text-md-start pt-5 pt-md-9">
+              <h1 class="display-2 fw-bold fs-4 fs-md-5 fs-xl-6">Building exactly the <br />eCommerce website you want.</h1>
+              <p class="mt-3 mb-4">WooCommerce is a customizable, open-source eCommerce platform built on WordPress.Get started quickly and make your way.</p><a class="btn btn-lg btn-info rounded-pill me-2" href="#" role="button">Start a New Store </a><span> or  </span><a class="btn btn-link ps-1 ps-md-4 ps-lg-1" href="#" role="button"> Customize &amp; Extend ›</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section class="py-6">
+
+        <div class="container">
+          <div class="row flex-center">
+            <div class="col-auto text-center my-4">
+              <h1 class="display-3 fw-bold">Your eCommerce<br /><span>made simple</span></h1>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4 mb-5 mb-md-0"><img class="img-fluid shadow-sm" src="<?=ASSETS . THEME?>/assets/img/gallery/Smart-Waste-Recycling-System-1-1000x600.jpg" alt="" />
+              <div class="mt-3 text-center text-md-start">
+                <h4 class="display-6 fs-2 fs-lg-3 fw-bold">All You Need to Start</h4>
+                <p class="mb-0">Add WooCommerce plugin to any WordPress site and set up a new store in minutes.</p><a class="btn btn-link ps-0" href="#" role="button"> Ecommerce Wordpress ›</a>
+              </div>
+            </div>
+            <div class="col-md-4 mb-5 mb-md-0"><img class="img-fluid shadow-sm" src="<?=ASSETS . THEME?>/assets/img/gallery/garbage-sorting-waste-transportation-innovative-green-technology-eco-smart-system-for-recycling-clipart-vector_csp96689551.webp" width="310" alt="" />
+              <div class="mt-3 text-center text-md-start">
+                <h4 class="display-6 fs-2 fs-lg-3 fw-bold">Customize and Extend</h4>
+                <p class="mb-0">From subscriptions to gym classes to luxury cars, WooCommerce is fully customizable.</p><a class="btn btn-link ps-0" href="#" role="button"> Browse Extensions › </a>
+              </div>
+            </div>
+            <div class="col-md-4 mb-5 mb-md-0"><img class="img-fluid shadow-sm" src="<?=ASSETS . THEME?>/assets/img/gallery/garbage-truck-trash-recycling-factory-waste-sorting-transport-vehicle-innovative-technology-isometric-illustration-green-eco-234230240.jpg" alt="" />
+              <div class="mt-3 text-center text-md-start">
+                <h4 class="display-6 fs-2 fs-lg-3 fw-bold">Active Community</h4>
+                <p class="mb-0">WooCommerce is one of the fastest-growing eCommerce communities. </p><a class="btn btn-link ps-0" href="#" role="button"> Check our Forums ›</a>
+              </div>
+            </div>
+          </div>
+        </div><!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+      <div class="shapeup">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path fill="#7854F7" fill-opacity="1" d="M0,64L120,69.3C240,75,480,85,720,85.3C960,85,1200,75,1320,69.3L1440,64L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+        </svg>
+      </div>
+      <section class="pt-4 pt-md-5 bg-info">
+        <div class="bg-holder" style="background-image:url(<?=ASSETS . THEME?>/assets/img/illustrations/about-bg.png);background-position:center center;background-size:contain;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row mb-5 text-center">
+            <div class="col-12 col-md-4 mb-4"><img class="img-fluid rounded-3 shadow-lg image-up" src="<?=ASSETS . THEME?>/assets/img/gallery/about-2.png" alt="" /></div>
+            <div class="col-12 col-md-8 text-md-start" style="max-width: 460px;">
+              <h3 class="fw-medium text-light"> Develop <br />Without Limits</h3>
+              <p class="text-light">WooCommerce is developer friendly, too. Built with a REST API, WooCommerce is scalable and can integrate with virtually any service. Design a complex store from scratch, extend a store for a client, or simply add a single product to a WordPress site—your store, your way.</p><a class="btn btn-lg btn-success rounded-pill" href="#">Read the Documentation</a>
+            </div>
+          </div>
+          <div class="row flex-center">
+            <div class="col-12 col-md-4 order-0 order-md-1"><img class="img-fluid rounded-3 shadow-lg image-down" src="<?=ASSETS . THEME?>/assets/img/gallery/about-1.png" alt="" /></div>
+            <div class="col-12 col-md-8 text-center text-md-start order-1 order-md-0" style="max-width: 460px;">
+              <h3 class="fw-medium text-light">Know our<br />Global Community</h3>
+              <p class="text-light">WooCommerce is one of the fastest-growing eCommerce communities. We’re proud that the helpfulness of the community and a wealth of online resources are frequently cited as reasons our users love it. There are 80+ meetups worldwide!</p><a class="btn btn-lg btn-success rounded-pill" href="#">Read the Documentation</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="shapedown">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path fill="#7854F7" fill-opacity="1" d="M0,256L120,250.7C240,245,480,235,720,234.7C960,235,1200,245,1320,250.7L1440,256L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path>
+        </svg>
+      </div>
+      <section class="py-7 bg-light-gradient">
+        <div class="bg-holder" style="background-image:url(<?=ASSETS . THEME?>/assets/img/gallery/testimonial-bg.png);background-position:center center;background-size:contain;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row flex-center mb-5">
+            <div class="col-auto text-center">
+              <h1 class="display-3 fw-bold fs-4 fs-md-6">Trusted by Agencies<br /><span>&amp; Store Owners</span></h1>
+            </div>
+          </div>
+          <div class="row flex-center">
+            <div class="col-12 col-md-9 col-lg-8 col-xl-7 col-xxl-6">
+              <div class="carousel slide carousel-fade" id="carouselExampleControls" data-bs-ride="carousel">
+                <div class="carousel-inner overflow-visible">
+                  <div class="card py-sm-6 px-sm-5 testimonial-card-shadow shadow-sm" style="transform: translateY(52px) scale(.9)"></div>
+                  <div class="card py-sm-6 px-sm-5 testimonial-card-shadow shadow-sm" style="transform: translateY(32px) scale(.934)"></div>
+                  <div class="card py-sm-6 px-sm-5 testimonial-card-shadow shadow-sm"></div>
+                  <div class="carousel-item z-index-1 active">
+                    <div class="card py-4 px-3 py-sm-6 px-sm-5 px-xl-7">
+                      <div class="bg-holder mt-4" style="background-image:url(<?=ASSETS . THEME?>/assets/img/gallery/q.png);background-position:center top;background-size:150px;">
+                      </div>
+                      <!--/.bg-holder-->
+
+                      <div class="card-body z-index-1">
+                        <p class="card-text fs-2 text-center">No other eCommerce platform allows people to start for free and grow their store as their business grows. More importantly, WooCommerce doesn't charge you a portion of your profits as your business grows!</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item z-index-1">
+                    <div class="card py-4 px-3 py-sm-6 px-sm-5 px-xl-7">
+                      <div class="bg-holder mt-4" style="background-image:url(<?=ASSETS . THEME?>/assets/img/gallery/q.png);background-position:center top;background-size:150px;height:168px;">
+                      </div>
+                      <!--/.bg-holder-->
+
+                      <div class="card-body z-index-1">
+                        <p class="card-text fs-2 text-center">WooCommerce doesn't charge you a portion of your profits as your business grows! No other eCommerce platform allows people to start for free and grow their store as their business grows. More importantly</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item z-index-1">
+                    <div class="card py-4 px-3 py-sm-6 px-sm-5 px-xl-7">
+                      <div class="bg-holder mt-4" style="background-image:url(<?=ASSETS . THEME?>/assets/img/gallery/q.png);background-position:center top;background-size:150px;height:168px;">
+                      </div>
+                      <!--/.bg-holder-->
+
+                      <div class="card-body z-index-1">
+                        <p class="card-text fs-2 text-center">More importantly, WooCommerce doesn't charge you a portion of your profits as your business grows! No other eCommerce platform allows people to start for free and grow their store as their business grows</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row mt-7 pe-6">
+                  <div class="col-12 position-relative text-end"><a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="bg-100 pb-0">
+        <div class="container">
+          <div class="row flex-center">
+            <div class="col-xl-5 text-center mb-5 z-index-1">
+              <h1 class="display-3 fw-bold fs-4 fs-md-6">Supported by real people</h1>
+              <p>Our team of Happiness Engineers works remotely from 58 countries providing customer support across multiple time zones.</p>
+            </div>
+          </div>
+        </div>
+        <div class="position-relative text-center">
+          <div class="bg-holder" style="background-image:url(undefined);background:url(<?=ASSETS . THEME?>/assets/img/gallery/people-bg-shape.png) no-repeat center bottom, url(<?=ASSETS . THEME?>/assets/img/gallery/people-bg-dot.png) no-repeat center bottom;">
+          </div>
+          <!--/.bg-holder-->
+          <img class="img-fluid position-relative z-index-1" src="<?=ASSETS . THEME?>/assets/img/gallery/people.png" alt="" />
+        </div>
+      </section>
+      <section class="py-0">
+        <div class="bg-holder z-index-2" style="background-image:url(<?=ASSETS . THEME?>/assets/img/illustrations/cta-bg.png);background-position:bottom right;background-size:61px 60px;margin-top:15px;margin-right:15px;margin-left:-58px;">
+        </div>
+        <!--/.bg-holder-->
+
+<?php $this->view("footer", $data);?>
+        
