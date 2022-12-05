2f99c3df2ed43a556e70558a023b5ef626a2feda
Nzola Kiampava <nzolakiampava@gmail.com>
2022-12-05 15:43:59 +0100

    Add files via upload

---
+++ app/views/waste/admin/includes/truck_modal.php
@@ -0,0 +1,194 @@
+<?php 
+  $User = $this->load_model('User');
+  $user_data = $User->check_login(true, ["Administrador"]);
+
+  if(is_object($user_data)){
+    $data['user_data'] = $user_data;
+  }
+  $DB = Database::newInstance();
+  $truck = $DB->read("SELECT * from colector_group");
+  $roots = $DB->read("SELECT * from garbage_address");
+?>
+<!-- ADD -->
+<!-- Modal -->
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+  <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              <h4 class="modal-title">Adicionar novo Caminhão</h4>
+          </div>
+          <div class="modal-body">
+            <form method="post">
+              <p>Entrar com Nome do Caminhão</p>
+              <input type="text" name="name" placeholder="Nome do caminhão" autocomplete="off" class="form-control placeholder-no-fix" required>
+              <p>Entrar com Matricula</p>
+              <input type="text" name="registration" placeholder="Matricula" autocomplete="off" class="form-control placeholder-no-fix" required>
+
+              <p>Selecionar Grupo </p>
+              <select name="group_id" class="form-control placeholder-no-fix" autocomplete="off" required>
+                <?php if(is_array($truck)):?>
+                    <?php foreach($truck as $item):?>
+                    <option value="<?=$item->id?>"><?=$item->group_name?></option>
+                    <?php endforeach;?>
+                <?php endif;?>
+              </select>
+
+              <p>Selecionar Primeiro endereço </p>
+              <select name="address_id_1" class="form-control placeholder-no-fix" autocomplete="off" required>
+                <?php if(is_array($roots)):?>
+                    <?php foreach($roots as $item):?>
+                    <option value="<?=$item->id?>"><?=$item->address?></option>
+                    <?php endforeach;?>
+                <?php endif;?>
+              </select>
+
+              <p>Selecionar Segundo endereço </p>
+              <select name="address_id_2" class="form-control placeholder-no-fix" autocomplete="off" required>
+                <?php if(is_array($roots)):?>
+                    <?php foreach($roots as $item):?>
+                    <option value="<?=$item->id?>"><?=$item->address?></option>
+                    <?php endforeach;?>
+                <?php endif;?>
+              </select>
+
+              <p>Selecionar Terceiro endereço </p>
+              <select name="address_id_3" class="form-control placeholder-no-fix" autocomplete="off" required>
+                <?php if(is_array($roots)):?>
+                    <?php foreach($roots as $item):?>
+                    <option value="<?=$item->id?>"><?=$item->address?></option>
+                    <?php endforeach;?>
+                <?php endif;?>
+              </select>
+
+
+            </div>
+            <div class="modal-footer">
+                <button data-dismiss="modal" class="btn btn-default" type="button"><i class="fa fa-close"></i> Cancelar</button>
+                <button class="btn btn-primary" name="add" type="submit"><i class="fa fa-save"></i> Salvar</button>
+            </div>
+          </form>
+      </div>
+  </div>
+</div>
+<!-- modal -->
+
+<!-- Edit -->
+<div class="modal fade" aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="edit">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title"><b>Editar Usuário</b></h4>
+          </div>
+          <div class="modal-body">
+            <form method="POST" action="">
+              <input type="hidden" class="userid" name="id">
+
+              <p>Email</p>
+              <input type="email" class="form-control placeholder-no-fix" autocomplete="off" placeholder="Email" id="edit_email" name="email" required>
+
+              <p>Nome</p>
+              <input type="text" class="form-control placeholder-no-fix" autocomplete="off" id="edit_name" name="name" required>
+
+              <p>Selecionar Perfil </p>
+              <select class="form-control placeholder-no-fix" name="rank">
+                <option selected id="rankselected"></option>
+                <option value="Administrador">Administrador</option>
+                <option value="Normal">Normal</option>
+              </select>
+          </div>
+          <div class="modal-footer">
+              <button data-dismiss="modal" class="btn btn-default" type="button"><i class="fa fa-close"></i> Cancelar</button>
+              <button class="btn btn-primary" name="edit" type="submit"><i class="fa fa-save"></i> Salvar</button>
+          </div>
+          </form>
+        </div>
+    </div>
+</div>
+
+<!-- Delete -->
+<div class="modal fade" id="delete">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b>Deletando...</b></h4>
+            </div>
+            <div class="modal-body">
+              <form class="form-horizontal" method="POST" action="">
+                <input type="hidden" class="userid" name="id">
+                <div class="text-center">
+                    <p>DELETAR USUÁRIO</p>
+                    <h2 class="bold fullname"></h2>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cancelar</button>
+              <button type="submit" class="btn btn-danger btn-flat" name="delete"><i class="fa fa-trash"></i> Deletar</button>
+              </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Update Photo -->
+<div class="modal fade" id="edit_photo">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b><span class="fullname"></span></b></h4>
+            </div>
+            <div class="modal-body">
+              <form class="form-horizontal" method="POST" enctype="multipart/form-data">
+                <input type="hidden" class="userid" name="id">
+                <div class="form-group">
+                    <label for="photo" class="col-sm-3 control-label">FOTO</label>
+
+                    <div class="col-sm-9">
+                      <input type="file" id="photo" name="photo" required>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cancelar</button>
+              <button type="submit" class="btn btn-success btn-flat" name="upload_photo"><i class="fa fa-check-square-o"></i> Atualizar</button>
+              </form>
+            </div>
+        </div>
+    </div>
+</div> 
+
+
+<!-- Activate -->
+<div class="modal fade" id="activate">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b>Activating...</b></h4>
+            </div>
+            <div class="modal-body">
+              <form class="form-horizontal" method="POST" action="users_activate.php">
+                <input type="hidden" class="userid" name="id">
+                <div class="text-center">
+                    <p>ACTIVATE USER</p>
+                    <h2 class="bold fullname"></h2>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+              <button type="submit" class="btn btn-success btn-flat" name="activate"><i class="fa fa-check"></i> Activate</button>
+              </form>
+            </div>
+        </div>
+    </div>
+</div> 
+
+
+     
