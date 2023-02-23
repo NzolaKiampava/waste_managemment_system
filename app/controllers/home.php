1cd2b1a3aa15280260f117fdec0adef83e402132
Nzola Kiampava <nzolakiampava@gmail.com>
2023-02-23 01:06:37 -1200

    Update home.php

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -7,11 +7,20 @@ Class Home extends Controller
 
 		$User = $this->load_model('User');
 		$user_data = $User->check_login();
+
+		$Message = $this->load_model('Message');
+
 		if(is_object($user_data)){
 			$data['user_data'] = $user_data;
 		}
 
+		if($_SERVER['REQUEST_METHOD'] == "POST")
+		{
+			$Message->insert_message($_POST, $_FILES);
+		}
+
 		$DB = Database::newInstance();
+		$data['messages'] = $DB->read("select * from messages order by id desc");
 		$data['page_title'] = "Home";
 		$this->view("index", $data);
 	}
